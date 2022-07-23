251d9e3bffbc99f8cdefe131bebb69b7bed92af8
Jan Tojnar <jtojnar@gmail.com>
2022-07-23 15:45:07 +0000

    gnome.devhelp: 41.2 → 43.alpha
    https://gitlab.gnome.org/GNOME/devhelp/-/compare/41.2...43.alpha

--- pkgs/desktops/gnome/devtools/devhelp/default.nix
+++ pkgs/desktops/gnome/devtools/devhelp/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchurl
-, fetchpatch
 , meson
 , ninja
 , pkg-config
@@ -22,23 +21,14 @@
 
 stdenv.mkDerivation rec {
   pname = "devhelp";
-  version = "41.2";
+  version = "43.alpha";
 
   outputs = [ "out" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/devhelp/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "7KqQsPTaqPsgMPbcaQv1M/+Zp3NDf+Dhis/oLZl/YNI=";
+    sha256 = "8HQxbo51vNjOWx+i1rn0GKrTTmtwt+u4JZkF5eVQjwI=";
   };
-
-  patches = [
-    # Fix build with meson 0.61
-    # https://gitlab.gnome.org/GNOME/devhelp/-/issues/59
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/devhelp/-/commit/281bade14c1925cf9e7329fa8e9cf2d82512c66f.patch";
-      sha256 = "LmHoeQ0zJwOhuasAUYy8FfpDnEO+UNfEb293uKttYKo=";
-    })
-  ];
 
   nativeBuildInputs = [
     meson
