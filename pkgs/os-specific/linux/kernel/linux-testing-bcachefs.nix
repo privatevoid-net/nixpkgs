662d2feb2a6bae91c63cf014abe7b8a0adce9b89
Madoura <madouura@gmail.com>
2022-07-31 22:19:06 -0500

    linux_testing_bcachefs: unstable-2022-04-25 -> unstable-2022-07-25

--- pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
+++ pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix
@@ -1,9 +1,10 @@
 { lib
+, pkgs
 , fetchpatch
 , kernel
-, date ? "2022-04-25"
-, commit ? "bdf6d7c1350497bc7b0be6027a51d9330645672d"
-, diffHash ? "09bcbklvfj9i9czjdpix2iz7fvjksmavaljx8l92ay1i9fapjmhc"
+, date ? "2022-07-25"
+, commit ? "5549e504645ac9ae570323eab6417b560a7def9b"
+, diffHash ? "sha256-DStuK3TSFB2BgVLMFxhL+q7RteURvYDj/RF14D5ENuw="
 , kernelPatches # must always be defined in bcachefs' all-packages.nix entry because it's also a top-level attribute supplied by callPackage
 , argsOverride ? {}
 , ...
@@ -17,19 +18,31 @@
     extraMeta = {
       branch = "master";
       maintainers = with lib.maintainers; [ davidak Madouura ];
-      broken = true;
     };
   } // argsOverride;
 
-  kernelPatches = [ {
+  kernelPatches = [
+    {
       name = "bcachefs-${commit}";
 
       patch = fetchpatch {
         name = "bcachefs-${commit}.diff";
         url = "https://evilpiepirate.org/git/bcachefs.git/rawdiff/?id=${commit}&id2=v${lib.versions.majorMinor kernel.version}";
         sha256 = diffHash;
+
+        postFetch = ''
+          ${pkgs.buildPackages.patchutils}/bin/filterdiff -x 'a/block/bio.c' "$out" > "$tmpfile"
+          mv "$tmpfile" "$out"
+        '';
       };
 
-      extraConfig = "BCACHEFS_FS m";
-    } ] ++ kernelPatches;
+      extraConfig = "BCACHEFS_FS y";
+    }
+
+    {
+      # Needed due to patching failure otherwise
+      name = "linux-bcachefs-bio.c-fix";
+      patch = ./linux-bcachefs-bio.c-fix.patch;
+    }
+  ] ++ kernelPatches;
 }))
