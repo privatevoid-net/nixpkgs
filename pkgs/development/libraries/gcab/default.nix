5ab616cdc108b63956630ea96d7b8ed709ba2501
Jan Tojnar <jtojnar@gmail.com>
2022-08-05 20:01:45 +0000

    gcab: 1.4 → 1.5
    https://gitlab.gnome.org/GNOME/gcab/-/compare/1.4...1.5

--- pkgs/development/libraries/gcab/default.nix
+++ pkgs/development/libraries/gcab/default.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gcab";
-  version = "1.4";
+  version = "1.5";
 
   outputs = [ "bin" "out" "dev" "devdoc" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "13q43iqld4l50yra45lhvkd376pn6qpk7rkx374zn8y9wsdzm9b7";
+    sha256 = "Rr90QkkfqkFIJCuewqB4al9unv+xsFZuUpDozIbwDww=";
   };
 
   patches = [
